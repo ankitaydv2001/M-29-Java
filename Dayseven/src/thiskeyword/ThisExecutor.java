7ef3862247b0caa176015d6a16e477a92de07f0d
Ankita Yadav <yadavankita497@gmail.com>
2023-04-26 05:54:08 -0700

    commit 7

---
+++ Dayseven/src/thiskeyword/ThisExecutor.java
@@ -0,0 +1,9 @@
+package thiskeyword;
+
+public class ThisExecutor {
+	 public static void main(String[] srgs) {
+		   ThisKeyExample t  = new ThisKeyExample(2323,123456789l);
+		   t.print();
+	   }
+
+}
---
+++ Dayseven/src/thiskeyword/ThisKeyExample.java
@@ -0,0 +1,27 @@
+package thiskeyword;
+
+public class ThisKeyExample {
+	//private data members
+		private int cvvNo;
+		private long atmNo;
+		
+		
+		//parameterized constructor
+		public ThisKeyExample(int cvvNo, long atmNo) {
+			/*It can be used to return the current 
+			 * class instance
+			 */
+		
+			this.cvvNo = cvvNo;
+			this.atmNo = atmNo;
+		}
+
+
+		//user-defined method
+		public void print()
+		{
+			System.out.println("CVV no is :"+cvvNo+" "
+					+"ATM Card No is:"+atmNo);
+		}
+
+}
